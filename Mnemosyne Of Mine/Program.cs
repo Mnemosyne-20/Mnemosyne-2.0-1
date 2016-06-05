9127577edc7eea51976e4bc885beec9e9af68131
chuggafan <chuggafans@gmail.com>
2016-06-05 15:38:57 -0400

    Added stuff and changed things

--- Mnemosyne Of Mine/Program.cs
+++ Mnemosyne Of Mine/Program.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Text.RegularExpressions;
 using RedditSharp;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
 namespace Mnemosyne_Of_Mine
 {
     class Program
@@ -59,10 +60,15 @@ namespace Mnemosyne_Of_Mine
                 createNewPath();
                 goto helper;
             }
+            //if(!File.Exists("D:\\RepliedToList.mdf")) //TODO: ADD THIS
+            //{
+            //    CreateDatabase();
+            //}
             if (Password == "Y")
             {
                 Console.WriteLine("Type in your password");
                 Password = Console.ReadLine();
+                Console.Clear();
             }
             Reddit reddit = null;
             if (Oauth != null)
@@ -198,6 +204,40 @@ namespace Mnemosyne_Of_Mine
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
                 Console.ReadKey();
+            }
+        }
+        /// <summary>
+        /// TODO: MAKE THIS WORK
+        /// </summary>
+        static void CreateDatabase()
+        {
+            string str;
+            SqlConnection connection = new SqlConnection();
+            str = "CREATE DATABASE RepliedTo ON PRIMARY " +
+                "(NAME = RepliedTo_Data, " +
+                "FILENAME = 'D:\\RepliedToList.mdf' " +
+                "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+                "FILENAME = 'D:\\RepliedToLog.ldf', " +
+                "SIZE = 1MB, " +
+                "MAXSIZE = 5MB, " +
+                "FILEGROWTH = 10%)";
+            SqlCommand command = new SqlCommand(str, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Database successfully created");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + '\n' + e.StackTrace);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
             }
         }
         /// <summary>
