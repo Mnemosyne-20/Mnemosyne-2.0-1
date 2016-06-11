1d35dc6149ecdeb671c7c2c775dd5827c44b1a3e
chuggafan <chuggafans@gmail.com>
2016-06-11 14:02:51 -0400

    Preparing to create a library specifically to archive, with exceptions built in, so any .net application can use them

---
+++ ArchiveLibrary/Archiving.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace ArchiveLibrary
+{
+    public class Archiving
+    {
+        /// <summary>
+        /// This pages through the comments of a post and tries to archive them
+        /// </summary>
+        /// <param name="postID">id for the post that you will get links in the comments and archive</param>
+        public static void Archive(string postID)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Gets the url of the Archive, goddamn once this is finished i will have no idea how this works
+        /// </summary>
+        /// <param name="serviceURL">Archiving service, generally archive.is</param>
+        /// <param name="url">The url that we're archiving</param>
+        /// <returns>the archive url</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "System.Console.WriteLine(System.String)")]
+        public static string Archive(string serviceURL, string url)
+        {
+            string archiveURL = null;
+            HttpClientHandler handle = new HttpClientHandler();
+            handle.AllowAutoRedirect = true;
+            using (var client = new HttpClient(handle))
+            {
+                var values = new Dictionary<string, string>
+                {
+                    {"url", url }
+                };
+                var content = new FormUrlEncodedContent(values);
+                serviceURL = "http://" + serviceURL + "/submit/";
+                /// <summary>
+                /// This puts a request to the archive site, so yhea...
+                /// </summary>
+                var response = client.PostAsync(serviceURL, content).Result;
+                archiveURL = response.RequestMessage.RequestUri.ToString();
+                if (archiveURL == "http://archive.is/submit/")
+                {
+                    #region fixing it
+                    StringReader reader = new StringReader(response.ToString());
+                    for (int i = 0; i < 3; i++)
+                    {
+                        reader.ReadLine();
+                    }
+                    string wanted = reader.ReadLine();
+                    reader.Dispose();
+                    string[] sides = wanted.Split('=');
+                    archiveURL = sides[1];
+                    #endregion
+                }
+            }
+            handle.Dispose();
+            return archiveURL;
+        }
+        /// <summary>
+        /// Making sure that we got the correct archive
+        /// </summary>
+        /// <param name="postPermalink">Permalink to fail post</param>
+        /// <param name="archiveURL">the url of the archive we recived</param>
+        /// <returns>wether or not it succeded</returns>
+        public static bool VerifyArchiveResult(string postPermalink, string archiveURL)
+        {
+            if (archiveURL == null || archiveURL == "http://archive.is/submit/")
+            {
+                throw new FailureToArchiveException($"Failed to archive: {postPermalink}, ");
+            }
+            return true;
+        }
+    }
+}
---
+++ ArchiveLibrary/FailureToArchiveException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveLibrary
+{
+    /// <summary>
+    /// Custom exception for when we fail to archive so that I can catch this and say "Oh!, this Failed!" in one central location
+    /// </summary>
+    public class FailureToArchiveException : Exception
+    {
+        public FailureToArchiveException() : base() { }
+        public FailureToArchiveException(string message) : base(message) { }
+        public FailureToArchiveException(string message, Exception inner) : base(message,inner) { }
+    }
+}
