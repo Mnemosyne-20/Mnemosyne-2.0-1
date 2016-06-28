7e09a016edff0eb71a132ce4ec94eb0e85bff8cf
chuggafan <chuggafans@gmail.com>
2016-06-28 12:26:49 -0400

    This is adding so much shit

--- Mnemosyne Of Mine/Config.cs
+++ Mnemosyne Of Mine/Config.cs
@@ -3,8 +3,15 @@ using System.Xml;
 
 namespace Mnemosyne_Of_Mine
 {
+    /// <remarks>
+    /// This is a partial because this is going to be worked on at multiple places with many functions to allow git to be able to push this properly, aswell as merge
+    ///</remarks>
     internal partial class Config
     {
+        /// <summary>
+        /// This is a method to convert the old file format into the new file format, will add a detection for it being new to old *soon*
+        /// </summary>
+        /// <param name="location">guess</param>
         public static void ConvertOldToNewData(string location)
         {
             UserData data = new UserData(location, true);
@@ -19,6 +26,23 @@ namespace Mnemosyne_Of_Mine
                 writer.WriteElementString("Username", data.Username);
                 writer.WriteAttributeString("Password",data.Password);
                 writer.WriteFullEndElement();
+                writer.WriteElementString("OAuth",data.bUseOAuth.ToString().ToLower());
+                writer.WriteAttributeString("Secret",data.OAuthClientSecret);
+                writer.WriteAttributeString("ID",data.OAuthClientID);
+                writer.WriteAttributeString("RedirURI",data.RedirectURI);
+                writer.WriteFullEndElement();
+                writer.WriteElementString("Subreddit",data.SubReddit);
+                writer.WriteFullEndElement();
+                writer.WriteElementString("ReqLimit", data.ReqLimit.ToString());
+                writer.WriteAttributeString("SleepTime", data.SleepTime.ToString());
+                writer.WriteStartElement("flavortext");
+                foreach(var text in data.FlavorText)
+                {
+                    writer.WriteString(text + "\n");
+                }
+                writer.WriteFullEndElement();
+                writer.WriteFullEndElement();
+                writer.WriteEndDocument();
             }
         }
     }
---
+++ Mnemosyne Of Mine/NConfigWriter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml;
+
+namespace Mnemosyne_Of_Mine
+{
+    internal partial class Config
+    {
+        static void WriteNewData(string location)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "\t";
+            settings.WriteEndDocumentOnClose = true;
+            using (XmlWriter writer = XmlWriter.Create(location, settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("settings");
+                writer.WriteAttributeString("New", true.ToString().ToLower());
+                Console.WriteLine("What is your username?");
+                writer.WriteElementString("Username", Console.ReadLine());
+                Console.WriteLine("Password? type Y if you want to type it in each time, as it's unencrypted");
+                writer.WriteAttributeString("Password", Console.ReadLine());
+                writer.WriteFullEndElement();
+                Console.WriteLine("Using OAuth?");
+                bool useOAuth = bool.Parse(Console.ReadLine().ToLower());
+                writer.WriteElementString("OAuth", useOAuth.ToString().ToLower());
+                string secret = null;
+                string id = null;
+                string redirectURI = null;
+                if(useOAuth)
+                {
+                    Console.WriteLine("What is the client secret?");
+                    secret = Console.ReadLine();
+                    Console.WriteLine("Client ID?");
+                    id = Console.ReadLine();
+                    Console.WriteLine("Redirect URI?");
+                    redirectURI = Console.ReadLine();
+                }
+                writer.WriteAttributeString("Secret", secret);
+                writer.WriteAttributeString("ID", id);
+                writer.WriteAttributeString("RedirURI", redirectURI);
+                writer.WriteFullEndElement();
+                Console.WriteLine("What subreddit will this be checking?");
+                writer.WriteElementString("Subreddit",Console.ReadLine());
+                writer.WriteFullEndElement();
+                Console.WriteLine("What is your perferred request limit?");
+                writer.WriteElementString("ReqLimit", Console.ReadLine());
+                Console.WriteLine("Time the bot sleeps after requesting a batch of data?");
+                writer.WriteAttributeString("SleepTime", Console.ReadLine());
+                writer.WriteStartElement("flavortext");
+                writer.WriteEndElement();
+                Console.WriteLine("Manually enter the flavortext, as that is too much to do here, delimited with a \\n");
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+    }
+}
