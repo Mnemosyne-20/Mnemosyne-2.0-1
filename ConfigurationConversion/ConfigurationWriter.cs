363ab4b4f75cf31e6e7a2ca934703b549a9e60e6
chuggafan <chuggafans@gmail.com>
2016-07-02 11:54:16 -0400

    Woooooo! unit test made + stuff done

--- ConfigurationConversion/ConfigurationWriter.cs
+++ ConfigurationConversion/ConfigurationWriter.cs
@@ -50,5 +50,51 @@ namespace ConfigurationConversion
             }
             #endregion
         }
+        /// <summary>
+        /// Only exists for the unit test
+        /// </summary>
+        /// <param name="location"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="usingOauth"></param>
+        /// <param name="ID"></param>
+        /// <param name="URI"></param>
+        /// <param name="secret"></param>
+        /// <param name="subreddit"></param>
+        /// <param name="reqlimit"></param>
+        /// <param name="sleeptime"></param>
+        /// <returns>string to test again</returns>
+        public static string WritseNewData(StringWriter location, string username, string password, bool usingOauth, string ID, string URI, string secret, string subreddit, string reqlimit, string sleeptime)
+        {
+            //XmlWriterSettings settings = new XmlWriterSettings();
+            //settings.Indent = true;
+            //settings.IndentChars = "\t";
+            //settings.WriteEndDocumentOnClose = true;
+            #region writer
+            using (XmlWriter writer = XmlWriter.Create(location))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("settings");
+                writer.WriteAttributeString("New", true.ToString().ToLower());
+                writer.WriteElementString("Username", username);
+                writer.WriteAttributeString("Password", password);
+                writer.WriteFullEndElement();
+                writer.WriteElementString("OAuth", usingOauth.ToString().ToLower());
+                writer.WriteAttributeString("Secret", secret);
+                writer.WriteAttributeString("ID", ID);
+                writer.WriteAttributeString("RedirURI", URI);
+                writer.WriteFullEndElement();
+                writer.WriteElementString("Subreddit", subreddit);
+                writer.WriteFullEndElement();
+                writer.WriteElementString("ReqLimit", reqlimit);
+                writer.WriteAttributeString("SleepTime", sleeptime);
+                writer.WriteStartElement("flavortext");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+            #endregion
+            return location.ToString();
+        }
     }
 }
