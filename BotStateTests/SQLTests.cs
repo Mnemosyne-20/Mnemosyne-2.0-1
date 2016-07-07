65d88034f1d472478cc3bd2902c40c1208575309
Spoot <spootacular@gmail.com>
2016-07-07 13:15:05 -0500

    Try to make unit tests runnable
    Some actually pass, others not so much.

--- BotStateTests/SQLTests.cs
+++ BotStateTests/SQLTests.cs
@@ -8,52 +8,40 @@ using System.Data.SQLite;
 namespace BotStateTests
 {
     [TestClass]
+    [DeploymentItem("x64\\SQLite.Interop.dll")] //it's half a bit stupid that this is even necessary, and another half a bit stupid that this specifically isn't deleted afterwards
     public class SQLTests
     {
-        IBotStateTracker SQLTracker;
-
-        Dictionary<string, string> BotRepliesDataset;
-        List<string> CheckedCommentsDataset;
+        Dictionary<string, string> BotRepliesDataset = new Dictionary<string, string>()
+        {
+            {"4nn36s", "d4yy5xu"},
+            {"4ntdcn", "d4yy63q"},
+            {"4ntao4", "d4yy6bj"}
+        };
+        List<string> CheckedCommentsDataset = new List<string>()
+        {
+            "d46qosm",
+            "d46qnw6",
+            "d46q8wt",
+            "d46q6ml",
+            "d46pwtk",
+            "d46k73i",
+            "d46ezlc"
+        };
         Dictionary<string, string> ArchivesDataset;
 
-        [TestInitialize]
-        public void PreTestSetup()
-        {
-            File.Copy("TestBase.sqlite", "Test.sqlite");
-            SQLTracker = new SQLBotStateTracker("Test.sqlite");
-            BotRepliesDataset = new Dictionary<string, string>()
-            {
-                {"4nn36s", "d4yy5xu"},
-                {"4ntdcn", "d4yy63q"},
-                {"4ntao4", "d4yy6bj"}
-            };
-            CheckedCommentsDataset = new List<string>()
-            {
-                "d46qosm",
-                "d46qnw6",
-                "d46q8wt",
-                "d46q6ml",
-                "d46pwtk",
-                "d46k73i",
-                "d46ezlc"
-            };
-        }
-
-        [TestCleanup]
-        public void PostTestCleanup()
-        {
-            File.Delete("Test.sqlite");
-        }
-
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "1")]
         public void BotReplyExistsTest_ReplyDoesNotExist()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("1\\Test.sqlite");
             Assert.IsFalse(SQLTracker.DoesBotCommentExist("wololo"));
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "2")]
         public void BotReplyExistsTest_ReplyDoesExist()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("2\\Test.sqlite");
             foreach (KeyValuePair<string, string> pair in BotRepliesDataset)
             {
                 Assert.IsTrue(SQLTracker.DoesBotCommentExist(pair.Key));
@@ -61,58 +49,74 @@ namespace BotStateTests
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "3")]
         public void BotReplyGet_ReplyExists()
         {
-            foreach(KeyValuePair<string, string> pair in BotRepliesDataset)
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("3\\Test.sqlite");
+            foreach (KeyValuePair<string, string> pair in BotRepliesDataset)
             {
                 Assert.AreEqual(pair.Value, SQLTracker.GetBotCommentForPost(pair.Key));
             }
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "4")]
         public void BotReplyGet_ReplyDoesNotExist()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("4\\Test.sqlite");
             Assert.IsNull(SQLTracker.GetBotCommentForPost("nope"));
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "5")]
         public void BotReplyAddTest_UniqueAdd()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("5\\Test.sqlite");
             SQLTracker.AddBotComment("yep", "blaah");
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "6")]
         [ExpectedException(typeof(SQLiteException))]
         public void BotReplyAddTest_NotUniqueAdd()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("6\\Test.sqlite");
             SQLTracker.AddBotComment("4nn36s", "d4yy5xu");
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "7")]
         public void CheckedCommentsTest_Unchecked()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("7\\Test.sqlite");
             Assert.IsFalse(SQLTracker.HasCommentBeenChecked("wololo"));
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "8")]
         public void CheckedCommentsTest_Checked()
         {
-            foreach(string str in CheckedCommentsDataset)
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("8\\Test.sqlite");
+            foreach (string str in CheckedCommentsDataset)
             {
                 Assert.IsTrue(SQLTracker.HasCommentBeenChecked(str));
             }
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "9")]
         public void CheckedCommentsTest_UniqueAdd()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("9\\Test.sqlite");
             SQLTracker.AddCheckedComment("unique");
         }
 
         [TestMethod]
+        [DeploymentItem("Test.sqlite", "10")]
         [ExpectedException(typeof(SQLiteException))]
         public void CheckedCommentsTest_NotUniqueAdd()
         {
+            IBotStateTracker SQLTracker = new SQLBotStateTracker("10\\Test.sqlite");
             SQLTracker.AddCheckedComment("d46qosm");
         }
     }
