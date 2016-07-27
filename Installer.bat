976423f06271ecefe34f7e77f4aac4cdb12ffa45
chuggafan <chuggafans@gmail.com>
2016-07-26 20:18:29 -0400

    WOOOOOHOOO window's .bat installation file, will have to figure out how to do mono

---
+++ Installer.bat
@@ -0,0 +1,6 @@
+.\nuget.exe restore .\Mnemosyne-Of-Mine.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe /m
+:: Holy shit how did I memorize that path name ::
+pause
+".\Mnemosyne Of Mine\bin\Debug\Mnemosyne Of Mine.exe"
+pause
--- Mnemosyne Of Mine/Program.cs
+++ Mnemosyne Of Mine/Program.cs
@@ -93,7 +93,7 @@ namespace Mnemosyne_Of_Mine
 #pragma warning disable CS0219 // Variable is assigned but its value is never used
             bool isMnemosyneThereAlready = false; // ignore visual studio complaining about this
 #pragma warning restore CS0219 // Variable is assigned but its value is never used
-            bool bDoPostArchiving = true; // temp off switch for archiving self posts themselves
+            bool bDoPostArchiving = false; // temp off switch for archiving self posts themselves
             #region postChecking
             while (true)
             {
